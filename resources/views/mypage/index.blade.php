37fba0d850fdd977821d7d65acaa1e03ba50dbcc
Hiroki-Fukutake <will.work.out87@gmail.com>
2019-11-16 23:12:20 +0900

    nt-mypage | マイページ(作業中)

--- resources/views/mypage/index.blade.php
+++ resources/views/mypage/index.blade.php
@@ -4,6 +4,27 @@
 @extends('layouts/front')
 @section('content')
         <div class="wrapper">
-            <img class="profile-img" src="{{asset('image/sample.jpg')}}">
+            @include('partials.mypage_left')
+            <div class="right-block fl-left">
+                <div class="mypost-area">
+                    <div class="content">
+                        <div class="content-top clearfix">
+                            <img class="fl-left content-prof-image" src="{{asset('image/sample.jpg')}}" alt="myimage">
+                            <p class="fl-left user-name">MYNAME@MYNAME</p>
+                        </div>
+                        <div class="text">
+                            <p>
+                                    女優の沢尻エリカ容疑者が、東京都内の自宅で合成麻薬のＭＤＭＡを所持していたとして、警視庁に逮捕されました。<br>「私の物に間違いない」と容疑を認めているということです。
+                            </p>
+                        </div>
+                        <div class="content-action">
+                            <i class="action-btn far fa-comment-dots"></i>
+                            <i class="action-btn far fa-thumbs-up"></i>
+                            <i class="action-btn far fa-share-square"></i>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
         </div>
 @endsection
---
+++ resources/views/partials/mypage_left.blade.php
@@ -0,0 +1,11 @@
+<div class="left-block fl-left">
+    <img class="profile-img" src="{{asset('image/sample.jpg')}}">
+    <div class="menu-area">
+        <a href="{{route('mypage.index')}}" class="mymenu">HOME</a>
+        <a href="javascript:void(0);" class="mymenu">プロフィール編集</a>
+        <a href="javascript:void(0);" class="mymenu">アクティビティ</a>
+        <a href="javascript:void(0);" class="mymenu">DM</a>
+        <a href="{{route('mypage.create')}}" class="mymenu">投稿する</a>
+        <a href="javascript:void(0);" class="mymenu">アカウント</a>
+    </div>
+</div>
