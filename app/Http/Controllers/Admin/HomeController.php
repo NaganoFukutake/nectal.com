144996c6b10a89f6cc5e749eb796d3cff806dd47
Hiroki-Fukutake <will.work.out87@gmail.com>
2019-11-23 20:32:33 +0900

    nt-admin | 管理画面 cookieのセットタイミング修正

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -8,7 +8,9 @@ use App\Http\Controllers\Controller;
 class HomeController extends Controller
 {
     public function index(Request $request) {
-
+        if (!isset($_COOKIE['admin-welcome'])) {
+            $_COOKIE['admin-welcome'] = false;
+        }
         return view('admin.home.index');
     }
 }
