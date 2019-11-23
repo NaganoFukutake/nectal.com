b0ac62bb650db18e574e4db06b7efcf3b9db4fb9
長野弘禎 <hirosada.n@gmail.com>
2019-11-23 20:08:27 +0900

    プロフィール編集画面とアクテビテー画面作成

--- resources/views/partials/mypage_left.blade.php
+++ resources/views/partials/mypage_left.blade.php
@@ -1,3 +1,5 @@
+
+
 <div class="left-block fl-left">
     <div class="auth-user-name">
         {{-- @if (Auth::guest()) --}}
@@ -7,8 +9,8 @@
     <img class="profile-img" src="{{asset('image/sample.jpg')}}">
     <div class="menu-area">
         <a href="{{route('mypage.index')}}" class="mymenu">HOME</a>
-        <a href="javascript:void(0);" class="mymenu">プロフィール編集</a>
-        <a href="javascript:void(0);" class="mymenu">アクティビティ</a>
+        <a href="{{route('mypage.edit', $user)}}" class="mymenu">プロフィール編集</a>
+        <a href="{{route('mypage.show', $user)}}" class="mymenu">アクティビティ</a>
         <a href="javascript:void(0);" class="mymenu">DM</a>
         <a href="{{route('mypage.create')}}" class="mymenu">投稿する</a>
         <a href="{{route('mypage.create')}}" class="mymenu">フォローリスト</a>
